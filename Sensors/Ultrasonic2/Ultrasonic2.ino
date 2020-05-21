70aad0d36e42480917f96b62d4053f3cbf558949
Diana <mdiannna@yahoo.com>
2020-05-21 15:06:12 +0300

    Final ultrasonic code

--- Sensors/Ultrasonic2/Ultrasonic2.ino
+++ Sensors/Ultrasonic2/Ultrasonic2.ino
@@ -1,28 +1,41 @@
 
 // defines pins numbers
-const int trigPin = 7;
-const int echoPin = 6;
+#define TRIG_PIN 7
+#define ECHO_PIN 6
+
 // defines variables
 long duration;
 int distance;
-void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
+
+void UltrasonicSetup() {
+  pinMode(TRIG_PIN, OUTPUT); // Sets the TRIG_PIN as an Output
+  pinMode(ECHO_PIN, INPUT); // Sets the ECHO_PIN as an Input
 }
+
+float GetDistance() {
+  // Clears the TRIG_PIN
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  // Sets the TRIG_PIN on HIGH state for 10 micro seconds
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  // Reads the ECHO_PIN, returns the sound wave travel time in microseconds
+  duration = pulseIn(ECHO_PIN, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  return distance;
+}
+
+void setup() {
+  Serial.begin(9600);
+  UltrasonicSetup();
+}
+
 void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+    float distance = GetDistance();
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println();
+    delay(400);
 }
