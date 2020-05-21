590d0b3e91da90fb48dd332f078f8ad3e6a5e3aa
Diana <mdiannna@yahoo.com>
2020-05-21 15:15:20 +0300

    PIR final code

--- Sensors/PIR2/PIR2.ino
+++ Sensors/PIR2/PIR2.ino
@@ -1,26 +1,48 @@
 // https://www.instructables.com/id/PIR-Motion-Sensor-With-Arduino-in-Tinkercad/
 
-int sensorState = 0;
+#define PIR_PIN 3
+
+void SetupPIR() {
+  pinMode(PIR_PIN, INPUT);
+}
+
+int ReadPIR() {
+  // read the state of the sensor/digital input
+  int sensorState = digitalRead(PIR_PIN);
+  return sensorState;
+}
 
 void setup()
 {
-  pinMode(2, INPUT);
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  SetupPIR();
+  // Init LED
+  //  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
-  // read the state of the sensor/digital input
-  sensorState = digitalRead(2);
   
-    // check if sensor pin is HIGH. if it is, set the
+  
+    Serial.print("PIR State: ");
+    int state = ReadPIR();
+    Serial.print(state);
+    if (state == HIGH) {
+      Serial.println();
+      Serial.print("MOVED!");
+    }
+    Serial.println();
+
+  // check if sensor pin is HIGH. if it is, set the
   // LED on.
-  if (sensorState == HIGH) {
-    digitalWrite(13, HIGH);
-    Serial.println("Sensor activated!");
-  } else {
-    digitalWrite(13, LOW);
-  }
-  delay(10); // Delay a little bit to improve simulation performance
+  //  if (sensorState == HIGH) {
+  //    digitalWrite(13, HIGH);
+  //    Serial.println("Moved!");
+  //    Serial.println("Sensor activated!");
+  //  } else {
+  //    digitalWrite(13, LOW);
+  // 
+  //}
+
+    delay(300); // Delay a little bit to improve simulation performance
 }
